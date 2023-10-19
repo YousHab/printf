5b0d2b6a9339e28ceff818ac4f3524ef9ad2214e
Youssef Haboul <haboulyousse14@gmail.com>
2023-10-19 06:13:52 +0100

    m

--- main.c
+++ main.c
@@ -3,14 +3,9 @@
 #include "main.h"
 int main(void)
 {
-	int len, len2;
-
-	len = _printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
-	len2 = printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
-	_putchar(48);
-	putchar(48);
-	printf("%c", 48);
-	_printf("%c", 48);
+	int len, len2;	
+	len = _printf("A char inside a sentence: %c. Did it work?\n", 'F');
+	len2 = printf("A char inside a sentence: %c. Did it work?\n", 'F');
 	fflush(stdout);
 	if (len != len2)
 	{
