d78f0893e626f7723a55c306b09bc23f5625f838
Youssef Haboul <haboulyousse14@gmail.com>
2023-10-21 01:32:08 +0100

    m

--- 7-printf.c
+++ 7-printf.c
@@ -1,32 +1,42 @@
 #include "main.h"
 /**
+ * print_S - function that prints non printable
+ *
+ * @args: the argument.
+ * Return: the count.
  *
  */
 
 int print_S(va_list args)
 {
-	const char *s;
+	char *s;
 	int count = 0;
+	int c;
 	
 	s = va_arg(args, char *);
-	_putchar('\\');
-	_putchar('x');
-	count = 2;
 	if (s == NULL)
 		s = "(null)";
 	while(*s)
 	{
 		if (*s < 32 || *s >= 127)
 		{
-			_putchar((*s >> 4) < 10 ? ('0' + (*s >> 4)) : ('A' + (*s >> 4) - 10));
-			_putchar((*s & 0x0F) < 10 ? ('0' + (*s & 0x0F)) : ('A' + (*s & 0x0F) - 10));
+			_putchar('\\');
+			_putchar('x');
 			count += 2;
+			c = *s;
+			if (c < 16)
+			{
+				_putchar('0');
+				count++;
+			}
+			count += print_va_HEX(c);
 		}
 		else
 		{
 			_putchar(*s);
 			count++;
 		}
+		s++;
 	}
 	return (count);
 }
